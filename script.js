309618e6c720c2d746cedfae48b94801b32cf906
Daiana <daianavieiradeandrade@gmail.com>
2022-04-21 15:31:52 -0300

    parte 3, 4 ok

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+//cria ordem aleatoria de cores
 let shuffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4); //floor arredonda o numero
     order[order.length] = colorOrder;
@@ -21,5 +22,43 @@ let shuffleOrder = () => {
 
     for (let i in order){
         let elementColor = createElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
     }
+}
+
+//acende a próxima cor
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number -250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    })
+}
+
+//checa s o0s botoes clicados sao os mesmos da ordem gerada no jogo
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if (clickedOrder[i] != order[i]){
+            lose();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert(`Pontuação: ${score}\nVocê acertou! Iniciando próximo nível!`);
+        nextLevel();
+    }
+}
+
+//funcao para o clique do usuario
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    elementColor(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+    })
+    
+    checkOrder();
 }
